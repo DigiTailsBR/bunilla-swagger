8f46735cef6ae715fe9948ecf05ce8f6a172ddb1
FlokiTV <dawysonw@gmail.com>
2024-07-21 09:36:09 -0300

    feat: add more methods on example

--- example/api/users/[id]/index.ts
+++ example/api/users/[id]/index.ts
@@ -17,6 +17,44 @@ paths.get("Get user by id", (route) => {
       },
     },
   });
+  route.response(400, {
+    description: "Bad request",
+  });
+});
+
+paths.put("Update user by id", (route, { $ref }) => {
+  route.response(200, {
+    description: "Get user",
+    content: {
+      "application/json": {
+        schema: $ref("User"),
+      },
+    },
+  });
+  route.response(400, {
+    description: "Bad request",
+  });
+});
+
+paths.delete("Remove user by id", (route, { $ref }) => {
+  route.response(200, {
+    content: {
+      "application/json": {
+        schema: {
+          type: "object",
+          properties: {
+            ok: {
+              type: "boolean",
+              example: "true",
+            },
+          },
+        },
+      },
+    },
+  });
+  route.response(400, {
+    description: "Bad request",
+  });
 });
 
 export const swagger = {
